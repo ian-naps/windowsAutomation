bfa5c46a0fe8578b5d5f0210cad1c5d3d8d58a91
Ian Napoli <ian.napoli8@gmail.com>
2022-03-03 16:13:22 -0800

    added function to get username

--- backup_user_data_script.py
+++ backup_user_data_script.py
@@ -1,6 +1,13 @@
-#windows 7 backup data script
+#windows 7 backup data script testing
 
 import os
 
-os.mkdir('C:\Users\inapoli\Desktop\test')
+
+#os.mkdir('C:\Users\inapoli\Desktop\test')
+def get_userName():
+    print("Please enter the user name to be transferred:\n")
+    userName = input()
+    print("Hello ", userName)
+    return userName
+
 
