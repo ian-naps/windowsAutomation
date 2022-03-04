ddc1654ab800699c540ca0f5ca4ea354e452be10
Ian Napoli <ian.napoli8@gmail.com>
2022-03-04 10:46:28 -0800

    added comments to batch script for printer

--- WAYC Konica Minolta Install Script/wayc-printer-konica.bat
+++ WAYC Konica Minolta Install Script/wayc-printer-konica.bat
@@ -1,1 +1,3 @@
-%SystemRoot%\explorer.exe \\coaeprt01\WAYCBIZ227
+Rem This program will install a network printer when executed. Change the \\networkprinter\printername to your printer 
+
+%SystemRoot%\explorer.exe \\networkprinter\printername
